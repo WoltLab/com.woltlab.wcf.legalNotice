9ce6ac8ef1ad62dbf9ee275fd2305abc5583b6c7
Matthias Schmidt <gravatronics@live.com>
2016-04-06 13:57:58 +0200

    Add constants.php for better IDE constant handling

---
+++ constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Defines constants for autocompletion in IDEs. This file is not meant to be actively used anywhere!
+ *
+ * @author	Matthias Schmidt
+ * @copyright	2001-2016 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.legalNotice
+ * @category	Community Framework
+ */
+
+// option constants
+define('LEGAL_NOTICE_ADDRESS', '');
+define('LEGAL_NOTICE_EMAIL_ADDRESS', '');
+define('LEGAL_NOTICE_PHONE', '');
+define('LEGAL_NOTICE_FAX', '');
+define('LEGAL_NOTICE_REPRESENTATIVE', '');
+define('LEGAL_NOTICE_REGISTER', '');
+define('LEGAL_NOTICE_VAT_ID', '');
+define('LEGAL_NOTICE_ADDITIONAL_TEXT', '');
+define('LEGAL_NOTICE_ADDITIONAL_TEXT_ENABLE_HTML', 0);
