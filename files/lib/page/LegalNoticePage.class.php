a2428538bc9bb13987a36b544938263391c21289
Marcel Werk <burntime@woltlab.com>
2016-06-15 11:16:37 +0200

    Renamed WCF to WSC / Removed obsolete phpdoc tags

--- files/lib/page/LegalNoticePage.class.php
+++ files/lib/page/LegalNoticePage.class.php
@@ -9,9 +9,7 @@ use wcf\util\StringUtil;
  * @author	Marcel Werk
  * @copyright	2001-2016 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package	com.woltlab.wcf.legalNotice
- * @subpackage	page
- * @category	Community Framework
+ * @package	WoltLabSuite\Core\Page
  */
 class LegalNoticePage extends AbstractPage {
 	const AVAILABLE_DURING_OFFLINE_MODE = true;
