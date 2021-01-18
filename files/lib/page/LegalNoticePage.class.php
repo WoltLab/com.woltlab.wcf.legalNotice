8e1e8b5d6963431746ab8eab0057b5ccb92d746e
Tim Düsterhus <duesterhus@woltlab.com>
2021-01-18 12:46:02 +0100

    Apply PSR-12 code style (#9)
    Co-authored-by: Alexander Ebert <ebert@woltlab.com>

--- files/lib/page/LegalNoticePage.class.php
+++ files/lib/page/LegalNoticePage.class.php
@@ -1,38 +1,43 @@
 <?php
+
 namespace wcf\page;
+
 use wcf\system\request\LinkHandler;
 use wcf\system\WCF;
 use wcf\util\StringUtil;
 
 /**
  * Shows the legal notice page.
- * 
- * @author	Marcel Werk
- * @copyright	2001-2019 WoltLab GmbH
- * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package	WoltLabSuite\Core\Page
+ *
+ * @author  Marcel Werk
+ * @copyright   2001-2019 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package WoltLabSuite\Core\Page
  */
-class LegalNoticePage extends AbstractPage {
-	const AVAILABLE_DURING_OFFLINE_MODE = true;
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function readParameters() {
-		parent::readParameters();
-		
-		$this->canonicalURL = LinkHandler::getInstance()->getLink('LegalNotice');
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function assignVariables() {
-		parent::assignVariables();
-		
-		WCF::getTPL()->assign([
-			'encodedEmailAddress' => StringUtil::encodeAllChars(LEGAL_NOTICE_EMAIL_ADDRESS),
-			'encodedUriEmailAddress' => StringUtil::encodeAllChars(rawurlencode(LEGAL_NOTICE_EMAIL_ADDRESS)),
-		]);
-	}
+class LegalNoticePage extends AbstractPage
+{
+    const AVAILABLE_DURING_OFFLINE_MODE = true;
+
+    /**
+     * @inheritDoc
+     */
+    public function readParameters()
+    {
+        parent::readParameters();
+
+        $this->canonicalURL = LinkHandler::getInstance()->getLink('LegalNotice');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function assignVariables()
+    {
+        parent::assignVariables();
+
+        WCF::getTPL()->assign([
+            'encodedEmailAddress' => StringUtil::encodeAllChars(LEGAL_NOTICE_EMAIL_ADDRESS),
+            'encodedUriEmailAddress' => StringUtil::encodeAllChars(\rawurlencode(LEGAL_NOTICE_EMAIL_ADDRESS)),
+        ]);
+    }
 }
